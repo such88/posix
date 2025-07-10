c32da2aeb5754dce7b1fba731344411bf4f269bc
suchLp <suchandandey@gmail.com>
2025-07-10 19:58:20 +0530

    pass arg from thread and return from thread

--- thread_feat_return_ip_arg/sum_arr_two_th.c
+++ thread_feat_return_ip_arg/sum_arr_two_th.c
@@ -2,17 +2,26 @@
 #include<stdlib.h>
 #include<pthread.h>
 #include<unistd.h>
-#define NO_OF_TH 10
-int primes[20]= {2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
-                        31, 37, 41, 43, 47, 53, 59, 61, 67, 71};
+#define NO_OF_TH 2
+int primes[10]= {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
 // This program demonstrates how to pass an argument to a thread in C using pthreads.
 
 void *routine(void *arg){
-    sleep(1);
     int index = *(unsigned int *)arg;
-    printf(" %d", primes[index]);
+    printf(" %d", index);
     free(arg); // Free the allocated memory for the argument
-    return NULL;
+    int *localsum = (int *) malloc(sizeof(int));
+    if(localsum == NULL){
+        perror("malloc");
+        exit(EXIT_FAILURE);
+    }
+    int sum = 0;
+    for(int i = 0; i < 5; i++){
+        sum += primes[i + index];
+    }
+    *localsum = sum;
+    printf("\nLocal sum : %d\n", *localsum);
+    return localsum;
 }
 
 int main(int argc, char *argv[]){
@@ -25,18 +34,25 @@ int main(int argc, char *argv[]){
             perror("malloc");
             exit(EXIT_FAILURE);
         }
-        *a = i; // Assign the index to the allocated memory
+        *a = 5* i; // Assign the index to the allocated memory
         if(pthread_create(&th[i], NULL, routine, a) != 0){
             perror("pthread_create");
             exit(EXIT_FAILURE);
         }
     }
-    for(i = 0; i < NO_OF_TH; i++){    
-        if(pthread_join(th[i], NULL) != 0){
+
+    int total_sum = 0;
+
+    for(i = 0; i < NO_OF_TH; i++){
+        int *r;
+        if(pthread_join(th[i], (void **)&r) != 0){
             perror("pthread_join");
             exit(EXIT_FAILURE);
         }
+        total_sum += *r;
+        free(r); // Free the memory allocated in the thread
     }
-    //pthread_join(th, NULL);
+
+    printf("\nTotal sum: %d\n", total_sum);
     return 0;
 }
